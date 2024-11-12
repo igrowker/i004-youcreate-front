74952da892365b9dc1704c0e3a3cd4eacc921f6d
Eliana Suancha <elianitasuanguz@gmail.com>
2024-11-12 16:16:45 -0500

    Adding dependencies to start the project and restructuring of folders

---
+++ src/router.tsx
@@ -0,0 +1,20 @@
+import  {FC} from "react";
+import {Routes, Route} from "react-router-dom";
+import {Login} from "./login.tsx";
+import {RouterLayout} from "./pages/common/RouterLayout";
+
+
+
+
+export const AppRouter: FC = () => {
+        return (
+            //esto lleva el layout de home en router layout  los componentes que lleven el menu y por fuera como el login que no muestra el navbar
+            <Routes>
+                <Route path="/" element={<RouterLayout/>}>
+
+                </Route>
+                <Route path="/login" element={<Login/>}/>
+            </Routes>
+        )
+    }
+;
