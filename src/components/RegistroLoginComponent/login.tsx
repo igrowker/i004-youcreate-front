d3c450cf26fdd43e5dad00cb993636d3e8b465a2
Eliana Suancha <elianitasuanguz@gmail.com>
2024-11-13 13:24:33 -0500

    login funcionalidades

--- src/components/RegistroLoginComponent/login.tsx
+++ src/components/RegistroLoginComponent/login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { FaGoogle, FaApple } from 'react-icons/fa';
+import { FaGoogle, FaApple } from 'react-icons/fa'; // Elimina el icono que no necesitas
 
 type LoginType = {
     email: string;
@@ -18,22 +18,28 @@ export const Login: React.FC = () => {
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
                 <div className="flex justify-center mb-6">
-                    <img className="w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/5087/5087579.png" alt="Login Logo" />
+                    <img className="w-20 h-20" src="https://cdn-icons-png.flaticon.com/512/6681/6681204.png" alt="Login Logo" />
                 </div>
                 <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
                     <div>
                         <h2 className="text-2xl font-semibold text-center">Iniciar sesión</h2>
+                        <p className="text-center text-sm">
+                            ¿No tenés una cuenta?{' '}
+                            <a href="#" className="text-blue-500 underline">
+                                Registrate acá
+                            </a>
+                        </p>
                         <input
-                            {...register('email', { required: 'Email is required' })}
+                            {...register('email', {required: 'Email is required'})}
                             type="email"
                             placeholder="Email"
-                            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            className="w-full p-3 mt-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                         />
                         {errors.email && <p className="mt-1 text-sm text-red-500">{errors.email.message}</p>}
                     </div>
-                    <div>
+                    <div className="mt-4">
                         <input
-                            {...register('password', { required: 'Password is required' })}
+                            {...register('password', {required: 'Password is required'})}
                             type="password"
                             placeholder="Contraseña"
                             className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -42,17 +48,12 @@ export const Login: React.FC = () => {
                     </div>
                     <button
                         type="submit"
-                        className="w-full p-3 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className="w-full p-3 mt-4 mb-4 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     >
                         Continuar
                     </button>
                 </form>
-                <p className="text-center text-sm">
-                    ¿No tenés una cuenta?{' '}
-                    <a href="#" className="text-blue-500 underline">
-                        Registrate acá
-                    </a>
-                </p>
+
                 <div className="flex items-center justify-center my-4">
                     <span className="w-full border-t border-gray-300"></span>
                     <span className="px-2 text-gray-500">o</span>
@@ -62,14 +63,14 @@ export const Login: React.FC = () => {
                     type="button"
                     className="flex items-center justify-center w-full p-3 mb-2 text-gray-800 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300"
                 >
-                    <FaGoogle className="w-5 h-5 mr-2" />
+                    <FaGoogle className="w-5 h-5 mr-2"/>
                     Continuar con Google
                 </button>
                 <button
                     type="button"
-                    className="flex items-center justify-center w-full p-3 text-gray-800 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300"
+                    className="flex items-center justify-center w-full p-3 mt-2 text-gray-800 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300"
                 >
-                    <FaApple className="w-5 h-5 mr-2" />
+                    <FaApple className="w-5 h-5 mr-2"/>
                     Continuar con Apple
                 </button>
             </div>
