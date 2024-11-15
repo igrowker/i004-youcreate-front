4e97b16ac1d3d4ff19628d034128b53055ed8f74
Alejo Andino <aleandino.cbaa@gmail.com>
2024-11-14 22:57:18 -0300

    BannerComponentLanding

---
+++ src/components/LandingPage/Banner.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import banner1 from '../../assets/imagesBanner/Banner01.png';
+import banner2 from '../../assets/imagesBanner/Banner02.png';
+import banner3 from '../../assets/imagesBanner/Banner03.png';
+
+const Banner: React.FC = () => {
+  
+  const images = [banner1, banner2, banner3];
+  const [currentImage, setCurrentImage] = useState(0);
+  const [fade, setFade] = useState(false);
+
+  // Cambiar la imagen cada 2 segundos
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setFade(false); // Inicia el fade-out
+      setTimeout(() => {
+        setCurrentImage(prev => (prev + 1) % images.length);
+        setFade(true); // Aplica el fade-in después del cambio de imagen
+      }, 500); // Tiempo para que dure el fade-out antes de cambiar la imagen
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="flex justify-center items-center px-48 mt-16">
+      <img
+        src={images[currentImage]}
+        alt="Banner"
+        className={`w-full h-full object-cover transition-opacity duration-500 ${fade ? 'opacity-100' : 'opacity-0'}`}
+      />
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Banner from "./Banner";
 
 const LandingPage: React.FC = () => {
   return (
     <div>
       <NavBar />
+      <Banner />
     </div>
   );
 };
