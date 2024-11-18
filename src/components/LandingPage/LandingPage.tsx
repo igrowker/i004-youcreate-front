2660120f8dc3ce295a995b8a2f6b5ee1f90286a5
Alejo Andino <aleandino.cbaa@gmail.com>
2024-11-18 13:53:50 -0300

    footerComponent

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import Banner from "./Banner";
 import Features from "./Features";
 import WhyUs from "./WhyUs";
+import Footer from "./Footer";
 
 const LandingPage: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const LandingPage: React.FC = () => {
       <Banner />
       <Features />
       <WhyUs />
+      <Footer />
     </div>
   );
 };
