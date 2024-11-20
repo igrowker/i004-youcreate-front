5d1c2530f8fdf553f302ebfc4726b3a1922b4484
Eliana Suancha <elianitasuanguz@gmail.com>
2024-11-20 10:17:32 -0500

    add a new login design according to the figma for ux

--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ import TwoFactorAuth from "./components/RegistroLoginComponent/TwoFactorAuth.tsx
 import NotFound from "./pages/NotFound.tsx";
 import VerifyAccountSuccess  from "./components/RegistroLoginComponent/VerifyAccountSuccess.tsx"; 
 import { ResetPassword } from "./components/RegistroLoginComponent/ResetPassword.tsx";
+import {Login1} from "./components/RegistroLoginComponent/login1.tsx";
 
 
 
@@ -22,6 +23,9 @@ export const AppRouter: FC = () => {
                     <Route index element={<LandingPage />} />
                 </Route>
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/login1" element={<Login1/>}/>
+
+
                 <Route path="/register" element={<Register/>}/>
                 <Route path="/reset-password" element={<ResetPassword/>}/>
 
