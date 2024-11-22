78e92e0eeb7a5bfbc76e247dafda977d93719713
joseRonald232001 <phjronald@gmail.com>
2024-11-22 01:22:09 -0400

    dashboard maquetacion inicial

--- src/components/secciones/Section4.tsx
+++ src/components/secciones/Section4.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Section4: React.FC = () => {
 
   return (
-    <div className='w-[500px] text-yellow-50 bg-slate-700'>
-    <h1>section 4</h1>
-  </div>
+    <div className='w-[500px] ml-10 p-4 text-yellow-50 bg-slate-700  m-auto h-[100px]'>
+      <br />
+      <h1 className='text-xl'>section 4</h1>
+    </div>
   );
 };
 
