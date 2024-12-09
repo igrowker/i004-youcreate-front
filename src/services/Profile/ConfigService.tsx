f53164463b26a4d10b73a2045fe4bd4daf4b89f3
GNNZVL19 <gonzavolante@gmail.com>
2024-12-09 14:16:58 -0300

    Profile y edicion finalizada

--- src/services/Profile/ConfigService.tsx
+++ src/services/Profile/ConfigService.tsx
@@ -1,24 +1,45 @@
 import axios from "axios";
+import { Profile } from "./ProfileSevice";
 const baseUrl = import.meta.env.VITE_API_URL;
 const user = JSON.parse(localStorage.getItem("loginData") as string);
 
 export interface ConfigProfile {
-    id: string;
+
     userName: string;
     lastName: string;
     email: string;
-    phone: string;
+    phoneNumber: string;
     country: string;
 }
 
-// Llamada a la API para actualizar el perfil del usuario
 export const updateUserProfile = async (profileData: ConfigProfile): Promise<void> => {
     try {
-        const response = await axios.put(`${baseUrl}/api/v1/user/${user.id}`, profileData, );
-        return response.data; // Puede ser un mensaje o cualquier otra respuesta que tu API devuelva
-    } catch (error) {
-        console.error("Error al actualizar el perfil:", error);
-        throw error; // Lanza el error para que sea manejado en el componente
+        const response = await axios.put(`${baseUrl}/api/v1/user/${user.id}`, profileData);
+        return response.data;
+    } catch (error: any) { // ❗️Usar 'any' puede generar errores no detectados
+        console.error("Error al actualizar el perfil:", error.response?.data || error.message);
+        throw error;
     }
 };
 
+// Función para convertir Profile a ConfigProfile
+export const mapProfileToConfigProfile = (profile: Profile): ConfigProfile => {
+    return {
+        userName: profile.userName,
+        lastName: profile.lastName,
+        email: profile.email,
+        phoneNumber: profile.phone, 
+        country: profile.country
+    };
+};
+
+export const getUserProfile = async (id:number): Promise<ConfigProfile> => {
+    try {
+        const response = await axios.get(`${baseUrl}/api/v1/user/${id}`);
+        console.log(response)
+        return response.data.data;
+    } catch (error) {
+        console.error("Error al obtener el perfil:", error);
+        throw error; // Lanza el error para ser manejado en el componente
+    }
+};
